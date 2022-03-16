f30e67eda7517c069cc6794066fafdf2ba77a3a5
Adam Kiss <iam@adamkiss.com>
2022-03-17 00:23:26 +0100

    Fix !isset to isset

--- icons/play-pause.php
+++ icons/play-pause.php
@@ -1,1 +1,1 @@
-<svg <?php if(!isset($class)) { echo "class=\"{$class}\" "; } ?>viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11 7H8V17H11V7Z" fill="currentColor"/><path d="M13 17H16V7H13V17Z" fill="currentColor"/></svg>
+<svg <?php if(isset($class)) { echo "class=\"{$class}\" "; } ?>viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11 7H8V17H11V7Z" fill="currentColor"/><path d="M13 17H16V7H13V17Z" fill="currentColor"/></svg>
--- icons/play-track-next.php
+++ icons/play-track-next.php
@@ -1,1 +1,1 @@
-<svg <?php if(!isset($class)) { echo "class=\"{$class}\" "; } ?>viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 17L14 12L6 7V17Z" fill="currentColor"/><path d="M18 7H15V12V17H18V7Z" fill="currentColor"/></svg>
+<svg <?php if(isset($class)) { echo "class=\"{$class}\" "; } ?>viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 17L14 12L6 7V17Z" fill="currentColor"/><path d="M18 7H15V12V17H18V7Z" fill="currentColor"/></svg>
